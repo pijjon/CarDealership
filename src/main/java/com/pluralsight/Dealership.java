27bdeb2fb9b4a2776d55cfde9f3256cfe222d2d3
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-27 10:27:56 -0400

    add Dealership constructor

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -8,5 +8,10 @@ public class Dealership {
     private String phone;
     private ArrayList<Vehicle> inventory;
 
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 
 }

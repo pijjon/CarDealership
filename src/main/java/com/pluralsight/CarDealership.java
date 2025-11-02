57b5ccfa171c2ef607cf6b5e8d079fb6b566ae91
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-02 02:56:25 -0500

    display UserInterface in main()

--- src/main/java/com/pluralsight/CarDealership.java
+++ src/main/java/com/pluralsight/CarDealership.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class CarDealership {
     public static void main(String[] args) {
-
+        UserInterface ui = new UserInterface();
+        ui.display();
     }
 }

6798884c7557d880e3b2b3cd373dca2077c0e956
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-01 23:03:33 -0400

    create skeleton structure for UserInterface class methods

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
 public class UserInterface {
+    private Dealership dealership;
+
+    public UserInterface(Dealership dealership) {
+        this.dealership = dealership;
+    }
+
+    public void display() {
+
+    }
+
+    public void processGetByPriceRequest() {
+
+    }
+
+    public void processGetByYearRequest() {
+
+    }
+
+    public void processGetByColorRequest() {
+
+    }
+
+    public void processGetByMileageRequest() {
+
+    }
+
+    public void processGetByVehicleTypeRequest() {
+
+    }
+
+    public void processAddVehicleRequest() {
+
+    }
+
+    public void processRemoveVehicleRequest() {
+
+    }
+
+
 }
